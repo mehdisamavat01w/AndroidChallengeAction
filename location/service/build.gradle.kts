6a9c005b30de324afdee048e85ad327e5d507f10
MS <mehdisamavat01@gmail.com>
2025-10-15 13:03:46 +0330

    feat(location): implement server module

---
+++ location/service/build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.android)
+}
+
+android {
+    namespace = "com.mahdisamavat.location.service"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_17
+        }
+    }
+}
+
+dependencies {
+    implementation(projects.core.common)
+    implementation(projects.core.logger)
+    implementation(projects.location.data)
+    implementation(projects.location.domain)
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.play.services.location)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+}
