db870933ff6d0c4124c81f24447509b4e295bdbf
Mahdi Samavat <mehdisamavat01@gmail.com>
2025-10-16 20:41:27 +0330

    fix(location-domain): add custom Result import

--- location/domain/src/main/java/com/mahdisamavat/location/domain/usecase/GetAllLocationsUseCase.kt
+++ location/domain/src/main/java/com/mahdisamavat/location/domain/usecase/GetAllLocationsUseCase.kt
@@ -4,6 +4,7 @@ import com.mahdisamavat.core.model.Location
 import com.mahdisamavat.location.domain.repository.LocationRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
+import com.mahdisamavat.core.common.result.Result
 
 
 class GetAllLocationsUseCase @Inject constructor(
--- location/domain/src/main/java/com/mahdisamavat/location/domain/usecase/GetLatestLocationUseCase.kt
+++ location/domain/src/main/java/com/mahdisamavat/location/domain/usecase/GetLatestLocationUseCase.kt
@@ -3,6 +3,7 @@ package com.mahdisamavat.location.domain.usecase
 import com.mahdisamavat.location.domain.repository.LocationRepository
 import com.mahdisamavat.core.model.Location
 import javax.inject.Inject
+import com.mahdisamavat.core.common.result.Result
 
 
 class GetLatestLocationUseCase @Inject constructor(
