ebf5379053097b29ee16867f4f6bfb1e47d60f1e
Mahdi Samavat <mehdisamavat01@gmail.com>
2025-10-19 02:22:08 +0330

    Clean up internet MainActivity

--- internet/app/src/main/java/com/mahdisamavat/internet/MainActivity.kt
+++ internet/app/src/main/java/com/mahdisamavat/internet/MainActivity.kt
@@ -4,17 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.CompositionLocalProvider
+import com.mahdisamavat.core.analytics.AnalyticsHelper
+import com.mahdisamavat.core.analytics.LocalAnalyticsHelper
 import com.mahdisamavat.internet.presentation.main.MainScreen
 import com.mahdisamavat.internet.ui.theme.AndoTheme
-
+import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
+    
+    private val analyticsHelper: AnalyticsHelper by inject()
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AndoTheme {
-                MainScreen()
+            CompositionLocalProvider(
+                LocalAnalyticsHelper provides analyticsHelper
+            ) {
+                AndoTheme {
+                    MainScreen()
+                }
             }
         }
     }
